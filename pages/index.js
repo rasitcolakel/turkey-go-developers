1e6987c9b0cabb70e7b4498b2dea689640c5e930
Raşit <rasitcolakel@hotmail.com>
2021-09-23 21:28:02 +0300

    toggle added for looking for job

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,10 @@
 import axios from "axios";
 import React from "react";
 import Head from "next/head";
+import Pagination from "../components/Pagination";
+import Developer from "../components/Developer";
+import Switch from "../components/Switch";
+
 export async function getServerSideProps({ req }) {
   const protocol = req.headers["x-forwarded-proto"] || "http";
   const baseUrl = req ? `${protocol}://${req.headers.host}` : "";
@@ -8,10 +12,15 @@ export async function getServerSideProps({ req }) {
   return { props: { data: response.data } };
 }
 
-import { HiOutlineChevronLeft, HiOutlineChevronRight } from "react-icons/hi";
-import { FiAlertCircle } from "react-icons/fi";
 export default function Post({ data }) {
+  const [developers, setDevelopers] = React.useState(data);
   const [page, setPage] = React.useState(0);
+  const [lookingForJobs, setLookingForJobs] = React.useState(false);
+  React.useEffect(() => {
+    setPage(0);
+    if (!lookingForJobs) setDevelopers([...data]);
+    else setDevelopers([...data.filter((d) => d[5]?.toUpperCase() === "EVET")]);
+  }, [lookingForJobs]);
   return (
     <div className="dev-container">
       <Head>
@@ -32,69 +41,21 @@ export default function Post({ data }) {
         </h1>
         <button></button>
       </div>
-
+      <div>
+        <Switch
+          checked={lookingForJobs}
+          setChecked={setLookingForJobs}
+          title={"Sadece İş arayanlar"}
+        />
+      </div>
       <div className="dev-grid">
-        <RenderDevelopers data={data.slice(0 + page * 12, (page + 1) * 12)} />
+        <Developer data={developers.slice(0 + page * 12, (page + 1) * 12)} />
       </div>
       <Pagination
         page={page}
         setPage={setPage}
-        maxPage={Math.ceil(data.length / 12)}
+        maxPage={Math.ceil(developers.length / 12)}
       />
-    </div>
-  );
-}
-
-function RenderDevelopers({ data }) {
-  return data.map((d, key) => <Developer d={d} />);
-}
-
-function Developer({ d }) {
-  const [, name, company, socialMedia, description, workStatus] = d;
-  let matches = socialMedia?.match(
-    /http(?:s)?:\/\/(?:www\.)?twitter\.com\/([a-zA-Z0-9_]+)/
-  );
-  let image = matches ? matches[1] : "GoTurkiye_";
-  let lookingForJobs = workStatus?.toUpperCase() === "EVET";
-  return (
-    <div className={`dev-card w-full ${lookingForJobs && "looking-for-jobs"}`}>
-      <img
-        className="w-1/2 rounded-full mx-auto"
-        src={"https://unavatar.io/" + image}
-      />
-      <h3 className="text-xl text-center py-2">{name}</h3>
-      <p className="text-lg text-center py-2">{company}</p>
-      <p className="text-sm text-center py-2 ">{description}</p>
-
-      {lookingForJobs && (
-        <div className="flex items-center justify-center text-xl text-center py-2 text-go">
-          <FiAlertCircle className="text-go mx-2 text-4xl" />{" "}
-          <span>İş Arıyor</span>
-        </div>
-      )}
-    </div>
-  );
-}
-
-function Pagination({ page, setPage, maxPage }) {
-  return (
-    <div className="flex items-center pt-2">
-      <button
-        disabled={page === 0}
-        type="button"
-        className="pagination-button"
-        onClick={() => setPage(page - 1)}
-      >
-        <HiOutlineChevronLeft />
-      </button>
-      <button
-        type="button"
-        className="pagination-button"
-        onClick={() => setPage(page + 1)}
-        disabled={page === maxPage - 1}
-      >
-        <HiOutlineChevronRight />
-      </button>
     </div>
   );
 }
