bafaecd30669f13d3f928dc7fd523aea9492660d
Raşit <rasitcolakel@hotmail.com>
2021-09-23 17:16:54 +0300

    bıktımmmmmmm

--- pages/api/developers/index.js
+++ pages/api/developers/index.js
@@ -1,21 +1,30 @@
 import { google } from "googleapis";
 export default async function getDevelopers(req, res) {
   const { method } = req;
+
   if (method === "GET") {
-    const auth = await google.auth.getClient({
-      scopes: ["https://www.googleapis.com/auth/spreadsheets.readonly"],
-    });
-    let id = 3;
-    let data;
-    const range = `Sheet1!A${parseInt(id)}:G${1000}`;
-    const sheets = google.sheets({ version: "v4", auth });
-    const response = await sheets.spreadsheets.values.get({
-      spreadsheetId: process.env.SHEET_ID,
-      range,
-    });
-    data = response.data.values.filter((d) => d[1]);
-    res.setHeader("Cache-Control", "s-maxage=30");
-    res.status(200).json(data);
+    try {
+      const target = ["https://www.googleapis.com/auth/spreadsheets.readonly"];
+      const jwt = new google.auth.JWT(
+        process.env.GOOGLE_SHEETS_CLIENT_EMAIL,
+        null,
+        (process.env.GOOGLE_SHEETS_PRIVATE_KEY || "").replace(/\\n/g, "\n"),
+        target
+      );
+      const range = `Sheet1!A${parseInt(3)}:G${1000}`;
+
+      const sheets = google.sheets({ version: "v4", auth: jwt });
+      const response = await sheets.spreadsheets.values.get({
+        spreadsheetId: process.env.SHEET_ID,
+        range: range, // sheet name
+      });
+
+      const data = response.data.values.filter((d) => d[1]);
+      res.setHeader("Cache-Control", "s-maxage=30");
+      res.status(200).json(data);
+    } catch (err) {
+      console.log(err);
+    }
   } else {
     res.setHeader("Allow", ["GET"]);
     res.status(405).end(`Method ${method} Not Allowed`);
