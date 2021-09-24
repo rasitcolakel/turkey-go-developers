fef876a5cf355dd46e7ba70cf61d3ca5f024a117
Raşit <rasitcolakel@hotmail.com>
2021-09-24 21:34:06 +0300

    api added for developers

--- pages/api/developers/[id].js
+++ pages/api/developers/[id].js
@@ -6,8 +6,9 @@ export default async function getDeveloper(req, res) {
     const protocol = req.headers["x-forwarded-proto"] || "http";
     const baseUrl = req ? `${protocol}://${req.headers.host}` : "";
     let response = await axios.get(`${baseUrl}/api/developers`);
-    let developer = response.data;
-    res.json(developer.filter((d) => d[0] === id)[0]);
+    let developer = response.data.filter((d) => d[0] === id)[0];
+    console.log(developer);
+    res.json(developer !== undefined ? developer : []);
   } else {
     res.setHeader("Allow", ["GET"]);
     res.status(405).end(`Method ${method} Not Allowed`);
